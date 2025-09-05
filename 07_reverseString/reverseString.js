6536158224408622c7b8d27cabe18ef9fe826c53
Debora S. Viana <deboraviana@alu.ufc.br>
2025-09-05 00:40:01 -0300

    add resolução do 07_reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let newWord = '';
+    for(let i = word.length; i >= 0; i--) {
+        newWord += word.charAt(i);
+    }
+    return newWord;
+    // return string.split("").reverse().join("");
+    // dessa forma transformaria cada caracter em um item de um array
+    // com o reverse, reverte todo o array
+    // com o join juntaria tudo novamente
 };
 
 // Do not edit below this line
