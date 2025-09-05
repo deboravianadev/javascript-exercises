768d13508d0f55805950ff092baab1b947581040
Debora S. Viana <deboraviana@alu.ufc.br>
2025-09-05 00:28:56 -0300

    add resolução do 06_repeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function (word, num) {
+    if (num < 0) {
+        return 'ERROR';
+    }
+    
+    let newWord = '';
 
+    for (let i = 0; i < num; i++) {
+        newWord += word;
+    }
+
+    return newWord;
 };
 
 // Do not edit below this line
