c9305b7e07056504ba11abac0fa21fcaf4d0ceff
Debora S. Viana <deboraviana@alu.ufc.br>
2025-09-05 01:05:09 -0300

    add resolução do 08_removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...num) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < num.length; j++) {
+            if (arr[i] === num[j]) {
+                arr.splice(i, 1);
+                i--;
+            }
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
